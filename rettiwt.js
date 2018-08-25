e213475919012668009a2130bc001743f1236323
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-08-25 10:08:41 +0000

    leet

--- rettiwt.js
+++ rettiwt.js
@@ -1,38 +1,47 @@
-let str_ = "た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎す͜͜͏̘̣͔͙͎͎ơ̟̤̖̗͖͇̍͋̀͆̓́͞͡け̜ͪ̅̍̅͂͊て";
-let char1 = "た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎";
-document.write(str_);
-document.write(char1.charCodeAt(0));
-document.write(String.fromCodePoint(0x40));
-document.write(String.fromCodePoint(0x61, 0x0359, 0x0359));
-document.write(String.fromCodePoint(97, 0x0359, 0x0359));
-document.write(String.fromCodePoint(97, 363, 363));
-document.write("a".codePointAt());
-let str = "This tweet is test.";
-document.write("<br>");
-document.write(str);
-for( let ch of str) {
-  //document.write(ch);
-  //document.write(ch.codePointAt());
-  document.write(String.fromCodePoint(ch.codePointAt(), 0x0359));
-  //document.write("<br>");
-};
+let leet = {};
+
+window.onload = function() {
+  leet.form = document.forms.form_leet;
+  leet.leet_text = document.getElementById("leet_text");
+
+  document.getElementById("form_leet_button").addEventListener("click", function() {
+    let input_str = leet.form._text.value;
+    let leet_str = "";
+    for( let ch of input_str ) {
+      let rand = Math.random() * 100;
+      if( rand > 70 )
+        leet_str += to_leet(ch);
+      else
+        leet_str += ch;
+    }
+    leet.leet_text.innerHTML = leet_str;
+  });
 
 
+  window.open('https://twitter.com/share?text='+ 'test' + '', '', 'scrollbars=yes, width=500, height=300, left=100, top=100,');
 
-document.write("<br>");
-document.write("<br>");
-document.write("<br>");
-document.write("<br>");
-document.write("<br>");
-document.write("<br>");
-let sss = "this sentence is test desu konnitiwa";
-for( let ch of sss ) {
-  let rand = Math.random() * 100;
-  if(rand > 70)
-    document.write(to_leet(ch));
-  else
-    document.write(ch);
 }
+
+// let str_ = "た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎す͜͜͏̘̣͔͙͎͎ơ̟̤̖̗͖͇̍͋̀͆̓́͞͡け̜ͪ̅̍̅͂͊て";
+// let char1 = "た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎";
+// document.write(str_);
+// document.write(char1.charCodeAt(0));
+// document.write(String.fromCodePoint(0x40));
+// document.write(String.fromCodePoint(0x61, 0x0359, 0x0359));
+// document.write(String.fromCodePoint(97, 0x0359, 0x0359));
+// document.write(String.fromCodePoint(97, 363, 363));
+// document.write("a".codePointAt());
+// let str = "This tweet is test.";
+// document.write("<br>");
+// document.write(str);
+// for( let ch of str) {
+//   //document.write(ch);
+//   //document.write(ch.codePointAt());
+//   document.write(String.fromCodePoint(ch.codePointAt(), 0x0359));
+//   //document.write("<br>");
+// };
+
+
 
 function to_leet(ch) {
   let leet = {'A': ['4', '@', '/'],
@@ -45,7 +54,7 @@ function to_leet(ch) {
               'H': ['#', '|-|', '9#'],
               'I': ['1', '!', '|'],
               'J': ['_|', '_/'],
-              'K': ['|<', 'X'],
+              'K': ['|&lt;', 'X'],
               'L': ['1', '|'],
               'M': ['|v|', 'AA', '|/|'],
               'N': ['||', '^/'],
