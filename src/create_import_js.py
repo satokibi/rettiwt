7cfdfbecda4cad138eb0a404df38745b4dc3bc97
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-08-27 00:12:06 +0900

    fix

--- src/create_import_js.py
+++ src/create_import_js.py
@@ -1,14 +1,14 @@
-
 def create_import_js(write_file, read_file_path):
-    with open(read_file, mode='r') as rfile:
-        wfile.write('function ' + read_file.split('.')[0] + '() {\n')
+    with open(read_file_path, mode='r') as rfile:
+        wfile.write('function ' + read_file_path.split('.')[0] + '() {\n')
         wfile.write("let html = '';")
-        pre_flag = False
+        # pre_flag = False
         while True:
             line = rfile.readline()
             if not line:
                 break
 
+            """
             if '<pre>' in line:
                 pre_flag = True
                 print(line)
@@ -20,13 +20,18 @@ def create_import_js(write_file, read_file_path):
                 wfile.write("html += '" + line.strip() + "\\n';")
             else:
                 wfile.write("html += '" + line.strip() + "';")
+            """
+            wfile.write("html += '" + line.strip() + "';")
         wfile.write("document.write(html);\n")
         wfile.write('}\n\n')
 
 
 if __name__ == '__main__':
     write_file = 'import.js'
-    read_file = 'body_rettiwt.html'
+
+    header_html_file = 'header.html'
+    footer_html_file = 'footer.html'
 
     with open(write_file, mode='w') as wfile:
-        create_import_js(wfile, read_file)
+        create_import_js(wfile, header_html_file)
+        create_import_js(wfile, footer_html_file)
