0331252bbaecadc8aa59daf9675d5a4788fe2c05
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-08-26 01:57:03 +0000

    added leet

---
+++ leet/leet.js
@@ -0,0 +1,81 @@
+let leet = {};
+
+window.onload = function() {
+  leet.form = document.forms.form_leet;
+  leet.leet_text = document.getElementById("leet_text");
+  leet.tweet_button = document.getElementById("tweet_button");
+
+  leet.leet_text.innerHTML = str_to_leet("Put your text in here to leet it.");
+
+  document.getElementById("form_leet_button").addEventListener("click", function() {
+    let input_str = leet.form._text.value;
+    let leet_str = str_to_leet(input_str);
+    leet.leet_text.innerHTML = leet_str;
+  });
+
+
+  leet.tweet_button.addEventListener("click", function() {
+    window.open('https://twitter.com/share?text='+ encodeURIComponent(leet.leet_text.innerHTML + '\nLeet変換'), '', 'scrollbars=yes, width=500, height=300, left=100, top=100,');
+
+    //window.open('https://twitter.com/intent/tweet?text=hello', '', 'scrollbars=yes, width=500, height=300, left=100, top=100,');
+  });
+};
+
+function str_to_leet(str) {
+  let leet_str = "";
+  for( let ch of str ) {
+    let rand = Math.random() * 100;
+    if( rand > 70 )
+      leet_str += to_leet(ch);
+    else
+      leet_str += ch;
+  }
+  return leet_str;
+}
+
+function to_leet(ch) {
+  let leet = {'A': ['4', '@', '/'],
+    'B': ['|3','8'],
+    'C': ['&lt;', '(', '['],
+    'D': ['|)', ')'],
+    'E': ['3', '&amp;'],
+    'F': ['|=', 'ph', '/='], 
+    'G': ['6', '9', '[,'],
+    'H': ['#', '|-|', '9#'],
+    'I': ['1', '!', '|'],
+    'J': ['_|', '_/'],
+    'K': ['|&lt;', 'X'],
+    'L': ['1', '|'],
+    'M': ['|v|', 'AA', '|/|'],
+    'N': ['||', '^/'],
+    'O': ['0', '()', '[]'],
+    'P': ['|*', '|o'],
+    'Q': ['&lt;|', '(_,)', '()_'],
+    'R': ['|2', '12'],
+    'S': ['5', '$'],
+    'T': ['7', '+'],
+    'U': ['(_)', '|_|'],
+    'V': ['/', '|/'],
+    'W': ['//', 'vv', 'uu'],
+    'X': ['&gt;&lt;', '}{'],
+    'Y': ['j', '7'],
+    'Z': ['2', '7_'],
+    '0': ['O', 'D'],
+    '1': ['I', 'L'],
+    '2': ['Z'],
+    '3': ['E'],
+    '4': ['h', 'A'],
+    '5': ['S'],
+    '6': ['b', 'G'],
+    '7': ['T', 'L', 'j'],
+    '8': ['B'],
+    '9': ['P'],
+  };
+
+  let ch_upper = ch.toUpperCase();
+  if(ch_upper in leet){
+    return leet[ch_upper][Math.floor(Math.random() * leet[ch_upper].length)];
+  } else{
+    return ch;
+  }
+}
--- rettiwt.js
+++ rettiwt.js
@@ -1,26 +1,6 @@
-let leet = {};
 
 window.onload = function() {
-  leet.form = document.forms.form_leet;
-  leet.leet_text = document.getElementById("leet_text");
-
-  document.getElementById("form_leet_button").addEventListener("click", function() {
-    let input_str = leet.form._text.value;
-    let leet_str = "";
-    for( let ch of input_str ) {
-      let rand = Math.random() * 100;
-      if( rand > 70 )
-        leet_str += to_leet(ch);
-      else
-        leet_str += ch;
-    }
-    leet.leet_text.innerHTML = leet_str;
-  });
-
-
-  window.open('https://twitter.com/share?text='+ 'test' + '', '', 'scrollbars=yes, width=500, height=300, left=100, top=100,');
-
-}
+};
 
 // let str_ = "た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎す͜͜͏̘̣͔͙͎͎ơ̟̤̖̗͖͇̍͋̀͆̓́͞͡け̜ͪ̅̍̅͂͊て";
 // let char1 = "た͜͜͏̘̣͔͙͎͎̘̜̫̗͍͚͓͜͜͏̘̣͔͙͎͎";
@@ -40,42 +20,3 @@ window.onload = function() {
 //   document.write(String.fromCodePoint(ch.codePointAt(), 0x0359));
 //   //document.write("<br>");
 // };
-
-
-
-function to_leet(ch) {
-  let leet = {'A': ['4', '@', '/'],
-              'B': ['|3','8'],
-              'C': ['&lt;', '(', '['],
-              'D': ['|)', ')'],
-              'E': ['3', '&amp;'],
-              'F': ['|=', 'ph', '/='], 
-              'G': ['6', '9', '[,'],
-              'H': ['#', '|-|', '9#'],
-              'I': ['1', '!', '|'],
-              'J': ['_|', '_/'],
-              'K': ['|&lt;', 'X'],
-              'L': ['1', '|'],
-              'M': ['|v|', 'AA', '|/|'],
-              'N': ['||', '^/'],
-              'O': ['0', '()', '[]'],
-              'P': ['|*', '|o'],
-              'Q': ['&lt;|', '(_,)', '()_'],
-              'R': ['|2', '12'],
-              'S': ['5', '$'],
-              'T': ['7', '+'],
-              'U': ['(_)', '|_|'],
-              'V': ['/', '|/'],
-              'W': ['//', 'vv', 'uu'],
-              'X': ['&gt;&lt;', '}{'],
-              'Y': ['j', '7'],
-              'Z': ['2', '7_'],
-          };
-
-  let ch_upper = ch.toUpperCase();
-  if(ch_upper in leet){
-    return leet[ch_upper][Math.floor(Math.random() * leet[ch_upper].length)];
-  } else{
-    return ch;
-  }
-}
