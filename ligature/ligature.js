4dbb2c203eee5601627a7084552c3d9e9e918df4
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-08-28 14:27:41 +0000

    copy button test

--- ligature/ligature.js
+++ ligature/ligature.js
@@ -2,6 +2,7 @@ window.onload = function() {
   ligature.form = document.forms.form_ligature;
   ligature.ligature_text = document.getElementById("ligature_text");
   ligature.tweet_button = document.getElementById("tweet_button");
+  ligature.coppy_button = document.getElementById("coppy_button");
   ligature.ligature_text.innerHTML = str_to_ligature("Put your text in here to lig it.");
   
 
@@ -13,6 +14,15 @@ window.onload = function() {
 
   ligature.tweet_button.addEventListener("click", function() {
     window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(ligature.ligature_text.innerHTML + '\nLigature\n' + location.href), '', 'scrollbars=yes, width=500, height=300, left=100, top=100,');
+  });
+
+  ligature.coppy_button.addEventListener("click", function() {
+    let str = ligature.ligature_text;
+    let range = document.createRange();
+    range.selectNode(str);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    alert('coppyしました');
   });
 };
 
