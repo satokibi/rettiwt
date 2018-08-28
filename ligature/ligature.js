03eed3f866a3002a8925d2bb320402129decf218
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-08-28 22:12:14 +0000

    fix translate button

--- ligature/ligature.js
+++ ligature/ligature.js
@@ -6,11 +6,13 @@ window.onload = function() {
   ligature.ligature_text.innerHTML = str_to_ligature("Put your text in here to lig it.");
   
 
-  document.getElementById("form_ligature_button").addEventListener("click", function() {
+  //document.getElementById("form_ligature_button").addEventListener("click", function() {
+  //document.getElementById("ligature_textarea").onchange = function() {
+  document.getElementById("ligature_textarea").onkeyup = function() {
     let input_str = ligature.form._text.value;
     let ligature_str = str_to_ligature(input_str);
     ligature.ligature_text.innerHTML = ligature_str;
-  });
+  };
 
   ligature.tweet_button.addEventListener("click", function() {
     window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(ligature.ligature_text.innerHTML + '\nLigature\n' + location.href), '', 'scrollbars=yes, width=500, height=300, left=100, top=100,');
